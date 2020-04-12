dec4931ed0babb5bccf2d1c35e7202cccda59446
Luke <lmschless@gmail.com>
2020-04-12 10:14:20 -0700

    abstracted TicketList out from app.js into it's own component.

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './header';
+
+function App() {
+	const name = 'Thato';
+	const name2 = 'Haley';
+	return (
+		<ReactFragment>
+			<Header />
+			<TicketList />
+		</ReactFragment>
+	);
+}
+
+export default App;

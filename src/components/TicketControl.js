aef25c6754e2d3f5b572d85e283fecda6e5d3872
Luke <lmschless@gmail.com>
2020-04-20 19:55:32 -0700

    working on ticketcontrol state variables.

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -6,10 +6,18 @@ export default class TicketControl extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {};
+		this.state = {
+			formVisableOnPage: false
+		};
 	}
 
 	render() {
-		return <div />;
+		let currentlyVisableState = null;
+		if (this.state.formVisibleOnPage) {
+			currentlyVisibleState = <NewTicketForm />;
+		} else {
+			currentlyVisibleState = <TicketList />;
+		}
+		return <React.Fragment>{currentlyVisibleState}</React.Fragment>;
 	}
 }

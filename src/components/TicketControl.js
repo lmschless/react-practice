c7af5ac7bfcf9220264492a91100b2307c0202fc
Luke <lmschless@gmail.com>
2020-04-20 21:58:34 -0700

    working on multi-page state for help queue.

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -6,7 +6,8 @@ export default class TicketControl extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			formVisableOnPage: false
+			formVisableOnPage: false,
+			clickCounter: 0
 		};
 	}
 
@@ -19,12 +20,39 @@ export default class TicketControl extends Component {
 	render() {
 		let currentlyVisibleState = null;
 		let buttonText = null;
+
+		if (this.state.clickCounter < 1) {
+			currentlyVisibleState = (
+				<p>
+					Have you gone through all the steps on the Learn How to Program debugging
+					lesson?
+				</p>
+			);
+			buttonText = 'Yes I have.';
+			this.setState((this.state.clickCounter: 1));
+		}
+		if (this.state.clickCounter < 2) {
+			currentlyVisibleState = <p>Have you asked another pair for help?</p>;
+			buttonText = 'Yes I have.';
+			this.setState((this.state.clickCounter: 2));
+		}
+		if (this.state.clickCounter < 3) {
+			currentlyVisibleState = (
+				<p>
+					Have you spent 15 minutes going through through the problem documenting
+					every step?
+				</p>
+			);
+			buttonText = 'Yes I have.';
+			this.setState((this.state.clickCounter: 3));
+		}
+
 		if (this.state.formVisibleOnPage) {
 			currentlyVisibleState = <NewTicketForm />;
 			buttonText = 'Return to Ticket List';
 		} else {
 			currentlyVisibleState = <TicketList />;
-			buttonText = 'Return to Ticket List';
+			buttonText = 'Add Ticket';
 		}
 		return (
 			<React.Fragment>

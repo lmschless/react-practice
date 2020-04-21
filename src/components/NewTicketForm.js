3fe56974b0597249c4808c879166b0c08be2678a
Luke <lmschless@gmail.com>
2020-04-20 22:55:52 -0700

    working multi-page react app with changing button and render states. Need to work on the form next.

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -3,7 +3,10 @@ import React from 'react';
 const NewTicketForm = (props) => {
 	return (
 		<div>
-			<h3>This is a form.</h3>
+			<h2>Fill out the form below to submit a help ticket!</h2>
+			<h4>Name: ___________</h4>
+			<h4>Location: ___________</h4>
+			<h4>Issue: __________</h4>
 		</div>
 	);
 };
--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -8,8 +8,16 @@ export default class TicketControl extends Component {
 		this.state = {
 			formVisableOnPage: false,
 			clickCounter: 0,
-			buttonText: 'Submit a Ticket',
-			currentlyVisibleState: <p>hi</p>
+			buttonText: 'Create a new ticket',
+			currentlyVisibleState: (
+				<React.Fragment>
+					<h3>Epicodus Help Queue Ticket Submission</h3>
+					<p>
+						Click below to create a ticket and a teacher will be right over to
+						assist!
+					</p>
+				</React.Fragment>
+			)
 		};
 	}
 
@@ -32,6 +40,26 @@ export default class TicketControl extends Component {
 			});
 			this.setState({ buttonText: 'Yes I have asked another pair.' });
 			this.setState({ clickCounter: this.state.clickCounter + 1 });
+		}
+		if (this.state.clickCounter === 2) {
+			this.setState({
+				currentlyVisibleState: (
+					<p>
+						Have you spent 15 minutes going through through the problem documenting
+						every step?
+					</p>
+				)
+			});
+			this.setState({
+				buttonText: 'Yes I tried multiple methods to solve the problem.'
+			});
+			this.setState({ clickCounter: this.state.clickCounter + 1 });
+		}
+		if (this.state.clickCounter === 3) {
+			this.setState({
+				currentlyVisibleState: <NewTicketForm />
+			});
+			this.setState({ buttonText: 'Submit Ticket' });
 		}
 	};
 
